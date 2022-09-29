5d94f0cb115c3c4815016809c8707ec6b0ff11ce
arun73822 <arun73822@gmail.com>
2022-09-29 11:00:27 +0530

    updated the prepare base model config

--- src/deep_classifier/entity/config_entity.py
+++ src/deep_classifier/entity/config_entity.py
@@ -13,3 +13,20 @@ class Data_Ingestion_Config:
     extracted_data_dir: Path
     ingested_train_dir: Path
     ingested_test_dir: Path
+
+@dataclass(frozen=True)
+class Prepare_Base_Model_Config:
+    prepare_base_model_dir: Path
+    base_model_dir: Path
+    base_model_file_name: str
+    updated_model_dir: Path
+    updated_model_file_name: str
+    params_image_size: list
+    params_agumentation: bool
+    params_batch_size: int
+    params_include_top: bool
+    params_epochs: int
+    params_classes: int
+    params_weights: str
+    params_learning_rate: float
+    
