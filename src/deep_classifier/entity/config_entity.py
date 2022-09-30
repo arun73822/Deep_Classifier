9873e6a7d3d92e3a01a6727e385097930807dcb8
arun73822 <arun73822@gmail.com>
2022-09-30 19:50:57 +0530

    model training and prepare _base model config

--- src/deep_classifier/entity/config_entity.py
+++ src/deep_classifier/entity/config_entity.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+from attr import frozen
+
 @dataclass(frozen=True)
 class Training_Pipeline_Config:
-    artifacr_dir: Path
+    artifact_dir: Path
 
 @dataclass(frozen=True)
 class Data_Ingestion_Config:
@@ -21,12 +23,27 @@ class Prepare_Base_Model_Config:
     base_model_file_name: str
     updated_model_dir: Path
     updated_model_file_name: str
-    params_image_size: list
-    params_agumentation: bool
-    params_batch_size: int
+    updated_model_file_path: Path
     params_include_top: bool
-    params_epochs: int
     params_classes: int
     params_weights: str
     params_learning_rate: float
+
+@dataclass(frozen=True)
+class Prepare_Callbacks_Config:
+    prepare_callbacks_dir: Path
+    tensorboard_log_dir: Path
+    model_checkpoint_dir: Path
+    model_checkpoint_file_name: str
+
+@dataclass(frozen=True)
+class Model_Training_Config:
+    model_training_dir: Path
+    model_training_file_name: str
+    updated_base_model_path: Path
     
+    training_data_path: Path
+    params_image_size: list
+    params_agumentation: bool
+    params_batch_size: int
+    params_epochs: int
